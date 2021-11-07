20b72bcafd67954b3a8077dff09ea8751497a9e0
helay2333 <2793146849@qq.com>
2021-11-07 12:50:30 +0800

    nodemon

---
+++ Demo/server.js
@@ -0,0 +1,74 @@
+//引用express
+const { response } = require("express");
+const express = require("express");
+
+//创建应用对象
+const app = express();
+
+//创建路由规则
+//request 是对请求报文的封装
+//response 是对响应报文的封装
+app.get("/server", (request, response) => {
+  //服务端get的路由规则
+  //设置响应头,设置允许跨域
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  //设置响应体
+  response.send("HELLO AJAX");
+});
+app.post("/server", (request, response) => {
+  //这是post的路由规则
+  //设置响应头,设置允许跨域
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "*");
+  //设置响应体
+  response.send("HELLO AJAX POSt");
+});
+app.all("/json-server", (request, response) => {
+  //设置响应头,设置允许跨域
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  //响应头
+  response.setHeader("Access-Control-Allow-Headers", "*");
+
+  //响应体
+  // response.send("HELLO AJAX ppppp");
+
+  //响应一个数据
+  const data = {
+    name: "aaaa",
+  };
+  //response.send()中必须放一个字符串或者buffer
+  //那么我们如果想要传递数据data就要对对象进行字符串转换
+  let str = JSON.stringify(data);
+  //设置响应体
+  response.send(str);
+});
+
+//IE
+app.get("/ie", (request, response) => {
+  //服务端get的路由规则
+  //设置响应头,设置允许跨域
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  //设置响应体
+  response.send("HELLO IE   -333ssssssss333333");
+});
+//延时
+app.get("/delay", (request, response) => {
+  //服务端get的路由规则
+  //设置响应头,设置允许跨域
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  setTimeout(() => {
+    response.send("延时响应");
+  }, 3000);
+  //设置响应体
+});
+//jQuery
+app.all("/jquery", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "*"); //允许自定义哦响应tou
+  // response.send("HELLO JQUery ");
+  const data = { name: "aaa" };
+  response.send(JSON.stringify(data));
+});
+app.listen(8000, () => {
+  console.log("服务已启动，8000端口监视中");
+});
